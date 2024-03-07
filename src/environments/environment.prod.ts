33e84355255ae303ca8cec30d79c50d3d0b37262
admin-chatbot <149711785+admin-chatbot@users.noreply.github.com>
2024-03-07 19:03:40 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  adminEndpoint: "http://lb-ecs-client-service-645136841.ap-south-1.elb.amazonaws.com/api/v1/admin/",
-  baseEndpoint: "http://lb-ecs-client-service-645136841.ap-south-1.elb.amazonaws.com/api/v1/",
+  adminEndpoint: "http://lb-service-client-service-uat-784897227.ap-south-1.elb.amazonaws.com/api/v1/admin/",
+  baseEndpoint: "http://lb-service-client-service-uat-784897227.ap-south-1.elb.amazonaws.com/api/v1/",
   consoleLog:"debug"
 };
