ec0b99f00c6086253de6042feb530d0c5e5fc513
“jitendrasagoriya” <jitendra.sagoriya@outlook.com>
2024-03-10 18:33:13 +0530

    Service detail

---
+++ src/app/components/main/service/detail/intent/intent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intent', 
+  templateUrl: './intent.component.html',
+  styleUrl: './intent.component.scss'
+})
+export class IntentComponent {
+
+}
